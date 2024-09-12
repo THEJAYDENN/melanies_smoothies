caeb1e31f96a4302cb45f1adb5cd95c4c243522f
THEJAYDENN <133974169+THEJAYDENN@users.noreply.github.com>
2024-09-12 17:22:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,6 @@ ingredients_list = st.multiselect(
 )
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
 
 if ingredients_list:
 
@@ -35,6 +32,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruitvice_response.json(), use_container_width=True)
         
     #st.write(ingredients_string)
 
